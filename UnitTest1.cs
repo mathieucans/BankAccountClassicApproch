d80d84889d725b6a6a71441276a06e838cf7f29c
CANS Mathieu <mathieu-externe.cans@edf.fr>
2018-08-20 14:25:38 +0200

    mock print test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,36 +9,115 @@ namespace BankAcountAllTest
 		[TestMethod]
 		public void TestAccountOperation()
 		{
-			var accountService = new AccountService();
+			var testPrinter = new TestPrinter();
+			var accountService = new AccountService(new AccountServiceImpl(), testPrinter);
+			
 			accountService.deposit(1000);
 			accountService.withdraw(100);
 			accountService.deposit(500);
 			accountService.printStatement();
-			string print = "";
+			string print = testPrinter.Print;
 			Assert.AreEqual(
-				"Date      | AMOUNT  | BALANCE" + "r\n" +
-			    "10/04/2014| 500.00  | 1400.00"+ "r\n" +
-				"02/04/2014| -100.00 | 900.00" + "r\n" +
+				"Date      | AMOUNT  | BALANCE" +"\r\n" +
+			    "10/04/2014| 500.00  | 1400.00"+ "\r\n" +
+				"02/04/2014| -100.00 | 900.00" + "\r\n" +
 				"01/04/2014| 1000.00 | 1000.00", 
 				print);
 		}
 	}
 
-	public class AccountService
+	public interface ITestPrinter
 	{
-		public void deposit(int p0)
+		void Write(string printStatement);
+	}
+
+	public class TestPrinter : ITestPrinter
+	{
+		public TestPrinter()
 		{
-			throw new NotImplementedException();
+			Print = "";
+		}
+		public string Print { get; private set; }
+
+		public void Write(string printStatement)
+		{
+			Print += printStatement;
+		}
+	}
+
+	[TestClass]
+	public class AccountServiceShould
+	{
+		[TestMethod]
+		public void print_deposeit_operation()
+		{
+			var accountService = new AccountServiceImpl();
+			accountService.deposit(1000);
+			var print = accountService.printStatement();
+			Assert.AreEqual(
+				"Date      | AMOUNT  | BALANCE" + "\r\n" +
+				"01/04/2014| 1000.00 | 1000.00",
+				print);
+		}
+
+		[TestMethod]
+		public void print_withdraw_operation()
+		{
+			var accountService = new AccountServiceImpl();
+			accountService.withdraw(100);
+			var print = accountService.printStatement();
+			Assert.AreEqual(
+				"Date      | AMOUNT  | BALANCE" + "\r\n" +
+				"01/04/2014| -100.00 | -100.00",
+				print);
+		}
+	}
+
+	public class AccountServiceImpl
+	{
+		private int account;
+
+		public void deposit(int i)
+		{
+			account += i;
+		}
+
+		public string printStatement()
+		{
+			return string.Format("Date      | AMOUNT  | BALANCE" + "\r\n" +
+			       "01/04/2014| {0}.00 | {0}.00", account);
 		}
 
 		public void withdraw(int i)
 		{
-			throw new NotImplementedException();
+			account -= i;
+		}
+	}
+
+	public class AccountService
+	{
+		private readonly AccountServiceImpl _accountServiceImpl;
+		private readonly ITestPrinter _printer;
+
+		public AccountService(AccountServiceImpl accountServiceImpl, ITestPrinter printer)
+		{
+			_accountServiceImpl = accountServiceImpl;
+			_printer = printer;
+		}
+
+		public void deposit(int p0)
+		{
+			_accountServiceImpl.deposit(p0);
+		}
+
+		public void withdraw(int i)
+		{
+			_accountServiceImpl.withdraw(i);
 		}
 
 		public void printStatement()
 		{
-			throw new NotImplementedException();
+			_printer.Write(_accountServiceImpl.printStatement());
 		}
 	}
 }
